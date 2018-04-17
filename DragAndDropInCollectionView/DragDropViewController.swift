8239b1d359ac75d0695704f0b705f657a83bacf0
Matteo Gavagnin <m@macteo.it>
2018-04-17 11:01:43 +0200

    Move multiple items from one collection to another

--- DragAndDropInCollectionView/DragDropViewController.swift
+++ DragAndDropInCollectionView/DragDropViewController.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
+struct Cell : Equatable {
+    var name : String
+    var list : Int = 0
+    
+    init(_ name: String) {
+        self.name = name
+    }
+    
+    public static func == (lhs: Cell, rhs: Cell) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
+
 class DragDropViewController: UIViewController
 {
     //MARK: Private Properties
     //Data Source for CollectionView-1
-    private var items1 = ["none", "chrome", "fade", "falseColor", "instant", "mono", "noir", "process", "sepia", "tonal", "transfer"]
+    
+    private var items1 = [Cell("none"), Cell("chrome"), Cell("fade"), Cell("falseColor"), Cell("instant"), Cell("mono"), Cell("noir"), Cell("process"), Cell("sepia"), Cell("tonal"), Cell("transfer")]
     
     //Data Source for CollectionView-2
-    private var items2 = [String]()
+    private var items2 = [Cell]()
 
     //MARK: Outlets
     @IBOutlet weak var collectionView1: UICollectionView!
@@ -35,7 +49,7 @@ class DragDropViewController: UIViewController
         self.collectionView2.dragInteractionEnabled = true
         self.collectionView2.dropDelegate = self
         self.collectionView2.dragDelegate = self
-        self.collectionView2.reorderingCadence = .fast //default value - .immediate
+        self.collectionView2.reorderingCadence = .immediate //default value - .immediate
     }
     
     //MARK: Private Methods
@@ -60,17 +74,19 @@ class DragDropViewController: UIViewController
                 if collectionView === self.collectionView2
                 {
                     self.items2.remove(at: sourceIndexPath.row)
-                    self.items2.insert(item.dragItem.localObject as! String, at: dIndexPath.row)
+                    self.items2.insert(item.dragItem.localObject as! Cell, at: dIndexPath.row)
                 }
                 else
                 {
                     self.items1.remove(at: sourceIndexPath.row)
-                    self.items1.insert(item.dragItem.localObject as! String, at: dIndexPath.row)
+                    self.items1.insert(item.dragItem.localObject as! Cell, at: dIndexPath.row)
                 }
                 collectionView.deleteItems(at: [sourceIndexPath])
                 collectionView.insertItems(at: [dIndexPath])
             })
             coordinator.drop(items.first!.dragItem, toItemAt: dIndexPath)
+        } else {
+            print("Multiple items")
         }
     }
     
@@ -89,15 +105,91 @@ class DragDropViewController: UIViewController
                 let indexPath = IndexPath(row: destinationIndexPath.row + index, section: destinationIndexPath.section)
                 if collectionView === self.collectionView2
                 {
-                    self.items2.insert(item.dragItem.localObject as! String, at: indexPath.row)
+                    self.items2.insert(item.dragItem.localObject as! Cell, at: indexPath.row)
                 }
                 else
                 {
-                    self.items1.insert(item.dragItem.localObject as! String, at: indexPath.row)
+                    self.items1.insert(item.dragItem.localObject as! Cell, at: indexPath.row)
                 }
                 indexPaths.append(indexPath)
             }
             collectionView.insertItems(at: indexPaths)
+        })
+    }
+    
+    
+    /// This method should transfer cells from source indexPath in one collection view to destination indexPath to another collection view, removing them from the source collection view. It should work for multiple items.
+    ///
+    /// - Parameters:
+    ///   - coordinator: coordinator obtained from performDropWith: UICollectionViewDropDelegate method
+    ///   - destinationIndexPath: indexpath of the collection view where the user drops the element
+    ///   - collectionView: collectionView in which reordering needs to be done.
+    private func transferItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView)
+    {
+        var coll1IndexPathsTBR = [IndexPath]()
+        var coll1IndexPathsTBA = [IndexPath]()
+        var items1TBR = [Cell]()
+        var items1TBA = [Cell]()
+        var coll2IndexPathsTBR = [IndexPath]()
+        var coll2IndexPathsTBA = [IndexPath]()
+        var items2TBR = [Cell]()
+        var items2TBA = [Cell]()
+        // collectionView.performBatchUpdates({
+            var indexPaths = [IndexPath]()
+            for (index, item) in coordinator.items.enumerated()
+            {
+                let indexPath = IndexPath(row: destinationIndexPath.row + index, section: destinationIndexPath.section)
+                let localObject = item.dragItem.localObject as! Cell
+                
+                if localObject.list == 0 {
+                    if let indexOf = items1.index(where: { $0.name == localObject.name }) {
+                        items1TBR.append(localObject)
+                        // items1.remove(at: indexOf)
+                        coll1IndexPathsTBR.append(IndexPath(item: indexOf, section: 0))
+                    }
+                } else {
+                    if let indexOf = items2.index(where: { $0.name == localObject.name }) {
+                        items2TBR.append(localObject)
+                        // items2.remove(at: indexOf)
+                        coll2IndexPathsTBR.append(IndexPath(item: indexOf, section: 0))
+                    }
+                }
+                
+                if collectionView === self.collectionView2
+                {
+                    var editedObject = localObject
+                    editedObject.list = 1
+                    items2TBA.append(editedObject)
+                    // self.items2.insert(editedObject, at: indexPath.row)
+                    coll2IndexPathsTBA.append(indexPath)
+                }
+                else
+                {
+                    var editedObject = localObject
+                    editedObject.list = 0
+
+                    items1TBA.append(editedObject)
+                    // self.items1.insert(editedObject, at: indexPath.row)
+                    coll1IndexPathsTBA.append(indexPath)
+                }
+                // indexPaths.append(indexPath)
+            }
+            // collectionView.insertItems(at: indexPaths)
+        // })
+        
+        items1 = items1.filter { !items1TBR.contains($0) }
+        items1.append(contentsOf: items1TBA)
+        
+        items2 = items2.filter { !items2TBR.contains($0) }
+        items2.append(contentsOf: items2TBA)
+        
+        collectionView1.performBatchUpdates({
+            collectionView1.deleteItems(at: coll1IndexPathsTBR)
+            collectionView1.insertItems(at: coll1IndexPathsTBA)
+        })
+        collectionView2.performBatchUpdates({
+            collectionView2.deleteItems(at: coll2IndexPathsTBR)
+            collectionView2.insertItems(at: coll2IndexPathsTBA)
         })
     }
 }
@@ -115,15 +207,15 @@ extension DragDropViewController : UICollectionViewDataSource
         if collectionView == self.collectionView1
         {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath) as! DragDropCollectionViewCell
-            cell.customImageView?.image = UIImage(named: self.items1[indexPath.row])
-            cell.customLabel.text = self.items1[indexPath.row].capitalized
+            cell.customImageView?.image = UIImage(named: self.items1[indexPath.row].name)
+            cell.customLabel.text = self.items1[indexPath.row].name.capitalized
             return cell
         }
         else
         {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath) as! DragDropCollectionViewCell
-            cell.customImageView?.image = UIImage(named: self.items2[indexPath.row])
-            cell.customLabel.text = self.items2[indexPath.row].capitalized
+            cell.customImageView?.image = UIImage(named: self.items2[indexPath.row].name)
+            cell.customLabel.text = self.items2[indexPath.row].name.capitalized
             return cell
         }
     }
@@ -135,7 +227,7 @@ extension DragDropViewController : UICollectionViewDragDelegate
     func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem]
     {
         let item = collectionView == collectionView1 ? self.items1[indexPath.row] : self.items2[indexPath.row]
-        let itemProvider = NSItemProvider(object: item as NSString)
+        let itemProvider = NSItemProvider(object: item.name as NSString)
         let dragItem = UIDragItem(itemProvider: itemProvider)
         dragItem.localObject = item
         return [dragItem]
@@ -144,7 +236,7 @@ extension DragDropViewController : UICollectionViewDragDelegate
     func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem]
     {
         let item = collectionView == collectionView1 ? self.items1[indexPath.row] : self.items2[indexPath.row]
-        let itemProvider = NSItemProvider(object: item as NSString)
+        let itemProvider = NSItemProvider(object: item.name as NSString)
         let dragItem = UIDragItem(itemProvider: itemProvider)
         dragItem.localObject = item
         return [dragItem]
@@ -180,7 +272,8 @@ extension DragDropViewController : UICollectionViewDropDelegate
             }
             else
             {
-                return UICollectionViewDropProposal(operation: .forbidden)
+                return UICollectionViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+                // return UICollectionViewDropProposal(operation: .forbidden)
             }
         }
         else
@@ -216,9 +309,11 @@ extension DragDropViewController : UICollectionViewDropDelegate
         case .move:
             self.reorderItems(coordinator: coordinator, destinationIndexPath:destinationIndexPath, collectionView: collectionView)
             break
-            
+
         case .copy:
-            self.copyItems(coordinator: coordinator, destinationIndexPath: destinationIndexPath, collectionView: collectionView)
+            // Those are mutually exclusive, you can copy or move between collectionViews
+            // self.copyItems(coordinator: coordinator, destinationIndexPath: destinationIndexPath, collectionView: collectionView)
+            self.transferItems(coordinator: coordinator, destinationIndexPath: destinationIndexPath, collectionView: collectionView)
             
         default:
             return
